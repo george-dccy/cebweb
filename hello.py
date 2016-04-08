b53357053a5c480bccf700b1998f8fd46a765aef
George Liu <george.dccy@gmail.com>
2016-04-08 23:31:28 +0800

    加入表单功能，重定向功能

--- hello.py
+++ hello.py
@@ -1,30 +1,55 @@
+# -*- coding:utf-8 -*-
+
 __author__ = 'George Liu'
-from flask import Flask, render_template
+
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'george-ldc'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash(u'你好像更改了用户名')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name)
+    return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+class NameForm(Form):
+    name = StringField(u'你的姓名是：', validators=[DataRequired()])
+    submit = SubmitField(u'提交')
+
 
 if __name__ == '__main__':
     manager.run()
