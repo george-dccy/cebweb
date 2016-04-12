9870a2aaf1fa25db2ae25d445d6f08be50e05d61
George Liu <george.dccy@gmail.com>
2016-04-12 22:31:29 +0800

    数据库配置1

--- hello.py
+++ hello.py
@@ -2,22 +2,30 @@
 
 __author__ = 'George Liu'
 
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'george-ldc'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -50,6 +58,21 @@ class NameForm(Form):
     name = StringField(u'你的姓名是：', validators=[DataRequired()])
     submit = SubmitField(u'提交')
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 if __name__ == '__main__':
     manager.run()
